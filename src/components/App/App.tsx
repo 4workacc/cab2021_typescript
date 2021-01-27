d076a75b82922f87ba18de1588d9c0c7454cd1a1
4workacc <58693140+4workacc@users.noreply.github.com>
2021-01-27 17:50:26 +0300

    creare cra-tsx

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,26 +1,10 @@
+import * as React from 'react';
 import './App.css';
 
-import { LoginPage } from '../LoginPage/LoginPage';
-// import MainPage  from '../MainPage/MainPage';
-
-import React, { useState, useEffect } from 'react';
-//import { useSelector } from 'react-redux'; 
-
-const App = () => {
- // const curStatePage = useSelector( state => state.curPage );
-  const [ curPage, switchPage ] = useState(<LoginPage />);
-
-  // useEffect (() => {
-  //   switch ( curStatePage ) {
-  //     case "LoginPage" : switchPage(<LoginPage />); break;
-  //     // case "MainPage"  : switchPage(<MainPage />); break;
-  //     default : switchPage(<LoginPage/>); break;
-  //   }
-  // }, [curStatePage])
-
+function App() {
   return (
     <div className="App">
-      {curPage}
+
     </div>
   );
 }
