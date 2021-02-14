d3c93716d2b3e9ddf2ea5889cd62d749ec0da7f0
eugen <4workacc@gmail.com>
2021-02-14 21:01:28 +0300

    admin test info load data

--- src/components/MainPage/AdminPages/AdminShowTestInfo.tsx
+++ src/components/MainPage/AdminPages/AdminShowTestInfo.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
 export const AdminShowTestInfo:React.FC = () => {
     const curShowTestInfoId = useSelector((state:IRootState) => state.curShowTestInfoId);
+    const [ curTestData, setCurTestData ] = useState<any>({});
+    useEffect(()=>{
+        fetch(`https://cab07.000webhostapp.com/new_refact/new_admin_GetTestInfo.php?request_test_id=${curShowTestInfoId}`)
+        .then((res) => res.json())
+        .then(
+          (result) => {
+                result.results.map ( (el:any) => {
+                    setCurTestData( el );
+                })
+           })
+    
+    },[curShowTestInfoId])
     return (
         <div className = "AdminShowTestInfo">
             { curShowTestInfoId}
+            { console.log(curTestData)}
         </div>
     )
 }
