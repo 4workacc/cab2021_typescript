6683a1d5622011c823a71a23701e5257bb43ad28
4workacc <58693140+4workacc@users.noreply.github.com>
2021-02-17 17:08:01 +0300

    AdminAddTask add active tasks

--- src/components/MainPage/AdminPages/AdminAddTask.tsx
+++ src/components/MainPage/AdminPages/AdminAddTask.tsx
@@ -4,22 +4,47 @@ import {
   InputLabel,
   makeStyles,
   MenuItem,
+  Paper,
   Select,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
 } from "@material-ui/core";
 import React, { useEffect, useState } from "react";
 
 const useStyles = makeStyles({
-    AdminAddTask : {
-        width: "100%",
-        heigth: "100%",
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-between"
-    },
-    select :{ 
-        width: "200px"
-    }
-})
+  AdminAddTask: {
+    width: "100%",
+    heigth: "20%",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingBottom: "50px"
+  },
+  select: {
+    width: "200px",
+    fontSize: "20px"
+  },
+  container: {  
+    width: "100%",
+    heigth: "100%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between"
+},
+tHead :{ 
+    width: "100%",
+    textAlign:"center",
+    fontSize: "30px"
+},
+  tc : {
+      width: "33%",
+      textAlign: "center"
+  }
+});
 
 export const AdminAddTask: React.FC = () => {
   const classes = useStyles();
@@ -29,6 +54,8 @@ export const AdminAddTask: React.FC = () => {
   const [selectedFIO, setSeletedFIO] = useState("");
   const [selectedTest, setSelectedTest] = useState("");
   const [selectedDate, setSelectedDate] = useState("");
+
+  const [ curActiveTasks, setCurActiveTasks ] = useState([]);
 
   const butClickHandler = () => {
     alert(`SET ${selectedTest} to user ${selectedFIO} before ${selectedDate}`);
@@ -41,6 +68,7 @@ export const AdminAddTask: React.FC = () => {
       .then((result) => {
         alert(result.message);
       });
+      getActiveTasks();
   };
 
   useEffect(() => {
@@ -52,48 +80,89 @@ export const AdminAddTask: React.FC = () => {
         setUsersFIO(result.users);
         setTestsname(result.tests);
       });
+      getActiveTasks();
   }, []);
-  return (
-    <div className={classes.AdminAddTask}>
-      <FormControl className = {classes.select}>
-        <InputLabel id="demo-simple-select-label">User</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={selectedFIO}
-          onChange={(e) => setSeletedFIO(e.target.value as string)}
-        >
-          {usersFIO.map((el) => {
-            return <MenuItem value={el}>{el}</MenuItem>;
-          })}
-        </Select>
-      </FormControl>
 
-      <FormControl className = {classes.select}>
-        <InputLabel id="demo-simple-select-label">Test</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          onChange={(e) => setSelectedTest(e.target.value as string)}
+  const getActiveTasks = () => {
+    fetch (`https://cab07.000webhostapp.com/new_refact/new_admin_getTasks.php`)
+    .then((res) => res.json())
+    .then((result) =>{
+        setCurActiveTasks( result.tasks);
+        // "task_user_fio" => $row["USER_FIO"],
+        // "task_test_name" => $row["TEST_NAME"],
+        // "task_end_time" => $row["END_DATE_TIME"]
+    })    
+  }
+  return (
+    <div className={classes.container}>
+      <div className={classes.AdminAddTask}>
+        <FormControl className={classes.select}>
+          <InputLabel id="demo-simple-select-label">User</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={selectedFIO}
+            onChange={(e) => setSeletedFIO(e.target.value as string)}
+          >
+            {usersFIO.map((el) => {
+              return <MenuItem value={el}>{el}</MenuItem>;
+            })}
+          </Select>
+        </FormControl>
+
+        <FormControl className={classes.select}>
+          <InputLabel id="demo-simple-select-label">Test</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            onChange={(e) => setSelectedTest(e.target.value as string)}
+          >
+            {testsName.map((el) => {
+              return <MenuItem value={el}>{el}</MenuItem>;
+            })}
+          </Select>
+        </FormControl>
+        <input
+          type="date"
+          value={selectedDate}
+          className={classes.select}
+          onChange={(e) => setSelectedDate(e.target.value as string)}
+        ></input>
+        <Button
+          className={classes.select}
+          onClick={butClickHandler}
+          variant="contained"
+          color="primary"
+          disabled={
+            selectedFIO === "" || selectedTest === "" || selectedDate === ""
+          }
         >
-          {testsName.map((el) => {
-            return <MenuItem value={el}>{el}</MenuItem>;
-          })}
-        </Select>
-      </FormControl>
-      <input
-        type="date"
-        value={selectedDate}
-        className = {classes.select}
-        onChange={(e) => setSelectedDate(e.target.value as string)}
-      ></input>
-      <Button 
-        className = {classes.select}
-        onClick={butClickHandler}
-        variant="contained" 
-        color="primary" 
-        disabled = { (selectedFIO==="")||(selectedTest==="")||(selectedDate==="") }
-        >Задаць тэст</Button>
+          Задаць тэст
+        </Button>
+      </div>
+      <p className = {classes.tHead}>Зададзена, але пакуль не выканана</p>
+      <Paper>
+        <TableContainer>
+          <Table stickyHeader aria-label="sticky table">
+            <TableHead>
+              <TableCell className = {classes.tc}>Фамілія</TableCell>
+              <TableCell className = {classes.tc}>Тэст</TableCell>
+              <TableCell className = {classes.tc}>Срок выканання</TableCell>
+            </TableHead>
+            <TableBody>
+                {curActiveTasks.map ((el:any) => {
+                    return (
+                        <TableRow>
+                            <TableCell className = {classes.tc}>{el.task_user_fio}</TableCell>
+                            <TableCell className = {classes.tc}>{el.task_test_name}</TableCell>
+                            <TableCell className = {classes.tc}>{el.task_end_time}</TableCell>
+                        </TableRow>                       
+                    )
+                })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Paper>
     </div>
   );
 };
